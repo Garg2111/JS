66692866217d58025df45ff8b6cdc4d06a3a73e1
ANSH GARG <124914967+Garg2111@users.noreply.github.com>
2025-07-01 08:06:43 +0000

    Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountID=144553
+let accountEmail="ansh@gmail.com"
+var accountPassword="12345" // espe scope ka problem rheta tha phele time me
+accoutCity="Sunam"
+let accountState;
+
+
+
+// accountID=2 //Not Allowed.
+accountEmail="hello@google.com"
+accountPassword="67890"
+accoutCity="Bangalore"
+
+console.log(accountID);
+
+
+/* 
+Prefer not to use var because of issue in block scope and functional scope. 
+*/ 
+
+console.table([accountEmail, accountID, accountPassword, accoutCity, accountState])
+
+
