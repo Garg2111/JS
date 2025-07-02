5ac5f49ed2f09d8ead4badf5361fcea9f9d1b12b
ANSH GARG <124914967+Garg2111@users.noreply.github.com>
2025-07-02 05:45:18 +0000

    Updated File.

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -48,8 +48,52 @@
 // "ansh" => true
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber= String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************OPERATIONS***********************
+
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**6);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" ansh"
+let str3=str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+let a=4
+let b=a++;
+const c=++a;
+console.log(b);
+console.log(c);
