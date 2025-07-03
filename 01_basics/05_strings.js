36a28476458699826f6502b55b043a79d5915860
ANSH GARG <124914967+Garg2111@users.noreply.github.com>
2025-07-03 04:44:57 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,38 @@
+const name="Ansh"
+const repoCount=15
+// console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName=new String("Ansh-Fc-new")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(4));
+
+// console.log(gameName.indexOf('F'));
+
+
+const newStsing=gameName.substring(0,4)
+// console.log(newStsing);
+
+// const anotherString=gameName.slice(-6,4)
+// console.log(anotherString);
+
+const newStringOne="   ansh   "
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+const url="https://ansh.com/ansh%20garg"
+
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('ansh'))
+
+console.log(gameName.split('-'));
