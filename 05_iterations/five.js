8a9ffa26a68d6599b2af23be6323f7b30b719054
ANSH GARG <124914967+Garg2111@users.noreply.github.com>
2025-07-05 07:12:21 +0000

    More About Loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,43 @@
+const coding = ["js","ruby","python","java","cpp"]
+
+coding.forEach(  function (item){
+    // console.log(item);
+})
+    
+
+coding.forEach((val) => {
+    // console.log(val);
+})
+    
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)    
+
+
+coding.forEach((item,index,arr) => {
+    // console.log(item,index,arr)
+})
+
+
+const myCoding = [
+    {
+        languageName:"Javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"Python",
+        languageFileName:"py"
+    },
+    {
+        languageName:"Java",
+        languageFileName:"java"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+})
+    
---
+++ 05_iterations/four.js
@@ -0,0 +1,31 @@
+const myObject = {
+    js:'javascript',
+    cpp:'C++',
+    rb:'ruby',
+    swift:'Swift by apple'
+}
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+ 
+
+// const programming = ["js","py","rb","cpp","java"]
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+    
+
+
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('AUS',"Australia")
+// for (const key in map) {
+//     console.log(key);
+// }
+
+
+
+
+   
